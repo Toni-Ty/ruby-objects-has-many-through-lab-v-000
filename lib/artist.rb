5716b7fecea0bb5f213b421b3d23c337028c06f5
Toni-Ty <antonique2479@yahoo.com>
2019-04-06 02:12:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Artist
 
 @@all = []
@@ -12,10 +13,32 @@ def self.all
   @@all
 end
 
-def new_song
-  new_song = Song.new(name, self, genre)
-  new_song
+def new_song (name, genre)
+  Song.new(name, self, genre)
 end
+
+def songs
+  Song.all.select { |song| song.artist == self }
+end
+
+def genres
+  self.songs.collect do |song|
+    song.genre
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
